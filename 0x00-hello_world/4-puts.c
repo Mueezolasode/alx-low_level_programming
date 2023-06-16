ad5f74d733fb03a5a008fbe26905a64975ce3bc8
Mueezolasode <olasodemueez2019@gmail.com>
2023-06-16 07:50:00 -0700

    sizes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/**
+ * main-entry point for the program.
+ *
+ * Return:0 if no errors, return non zero if errors
+ */
 int main(void)
-{ 
-char Mystr[] = "\"Programming is like building a multilingual puzzle";    
+{
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
 puts(Mystr);
 return (0);
 }
