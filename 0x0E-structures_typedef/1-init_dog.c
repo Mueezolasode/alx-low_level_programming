7ed95e299529ca045e402645e7a8374bbf799b4e
Mueezolasode <olasodemueez2019@gmail.com>
2023-07-18 14:18:34 -0700

    dogs

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+* init_dog - initializes a struct
+* @d: pointer to strut
+* @name: point to the name of the dog
+* @age: age of dog
+* @owner: point to owner
+*
+* Return:Return always 0
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c~
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+* free_dog - frees dogs
+* @d: dog to free pointer
+* Return: 0
+*/
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
